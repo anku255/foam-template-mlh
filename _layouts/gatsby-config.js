ab302da53638976d3966e619b79749ed9fc47174
chirag-singhal <csinghal208@gmail.com>
2020-07-16 20:32:17 +0530

    added new template

---
+++ _layouts/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: `/foam-gatsby-template`,
+  siteMetadata: {
+    title: `Foam`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [`gatsby-remark-katex`],
+      },
+    },
+    {
+      resolve: `gatsby-theme-garden`,
+      options: {
+        rootNote: "/readme",
+        contentPath: `${__dirname}/..`,
+        mdxOtherwiseConfigured: true,
+        ignore: [
+          "**/_layouts/**",
+          "**/.git/**",
+          "**/.github/**",
+          "**/.vscode/**",
+        ],
+      },
+    },
+  ],
+};
